dd9ab1a3d04b8f4fc392783cf460b0b6ab6d9d9f
PraveenTech005 <praveentech005@gmail.com>
2025-08-30 11:21:33 +0530

    Server CORS Update

--- server/server.js
+++ server/server.js
@@ -2,13 +2,26 @@ const express = require("express");
 const cors = require("cors");
 const { connectDB } = require("./config/DB");
 const userRoute = require("./routes/user.Route");
-const {hashpass, checkPass} = require("./helpers/helper");
 
 const PORT = process.env.PORT || 3000;
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://login-signup-kappa-fawn.vercel.app",
+];
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, cb) => {
+      if (!origin || allowedOrigins.includes(origin)) return cb(null, true);
+      return cb(new Error("Not Allowed by CORS"));
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 connectDB();
 
